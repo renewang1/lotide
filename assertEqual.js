914b54f38c5e52444ef7681edd37dfe38d413f69
Rene Wang <rene.wang1@hotmail.com>
2020-11-23 19:12:20 +0000

    Created assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('🛑🛑🛑 Assertion passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('✅✅✅ Assertion failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(2, 1);
