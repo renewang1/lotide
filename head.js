67f10cd05e83717f50a285f3d593b289fd684a22
Rene Wang <rene.wang1@hotmail.com>
2020-11-23 19:29:36 +0000

    Created head function

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  return array[0];
+}
+
+assertEqual(head([5]), 5);
+assertEqual(head([]), undefined);
