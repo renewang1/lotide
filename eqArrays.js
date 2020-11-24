30bb1a7972a5f3ce51eb000b5bccdd7ab7ee9c96
Rene Wang <rene.wang1@hotmail.com>
2020-11-24 20:21:46 +0000

    Added function eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let equal = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      equal = false;
+    }
+  }
+  return equal;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([], []), true); // => should PASS
+assertEqual(eqArrays([3, 2, 1], [1, 2, 3]), false); // => should FAIL
