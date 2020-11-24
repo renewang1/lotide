ce293b88eae30c1fde96842a26cc5103abe80f81
Rene Wang <rene.wang1@hotmail.com>
2020-11-24 20:30:17 +0000

    Fixed formatting with eslint and removed assertEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   let equal = true;
   for (let i = 0; i < array1.length; i++) {
@@ -14,7 +6,7 @@ const eqArrays = function(array1, array2) {
     }
   }
   return equal;
-}
+};
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -22,6 +14,6 @@ const assertArraysEqual = function(array1, array2) {
   } else {
     console.log('These arrays are not equal!');
   }
-}
+};
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
