6881f589eada778881e965e7b9befe0d34fac005
Rene Wang <rene.wang1@hotmail.com>
2020-11-25 14:45:50 +0000

    Added eqArrays and assertArraysEqual to middle.js

---
+++ middle.js
@@ -0,0 +1,19 @@
+const eqArrays = function(array1, array2) {
+  let equal = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      equal = false;
+    }
+  }
+  return equal;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log('These arrays are equal!');
+  } else {
+    console.log('These arrays are not equal!');
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
