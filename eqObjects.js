5f0f2b9ea0ae002e69fb4c54ebb36c8bc339f308
Rene Wang <rene.wang1@hotmail.com>
2020-11-25 20:54:22 +0000

    fixed bugs and added test cases

--- eqObjects.js
+++ eqObjects.js
@@ -6,30 +6,46 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(array1, array2) {
+  let equal = true;
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return equal;
+};
+
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (const key in object1) {
-      if (object1[key] !== object2[key]) {
+      if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (object1[key] !== object2[key]) {
         return false;
       }
     }
+    return true;
   } else {
     return false;
   }
-  return true;
 };
-
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+assertEqual(eqObjects(ab, abc), false); // => false
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-eqObjects(cd, dc); // => true
+assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-eqObjects(cd, cd2); // => false
+assertEqual(eqObjects(cd, cd2), false); // => false
