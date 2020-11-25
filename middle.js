c457f385e01ed8520465f4af197787645e121d8b
Rene Wang <rene.wang1@hotmail.com>
2020-11-25 14:59:18 +0000

    Completed middle.js function and added some test cases

--- middle.js
+++ middle.js
@@ -16,4 +16,29 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+const middle = function(array) {
+  let middle = [];
+  if (array.length <= 2) {
+    return middle;
+  } else if (array.length % 2 === 0) {
+    middle = array.slice(array.length / 2 - 1, array.length / 2 + 1);
+    return middle;
+  } else if (array.length % 2 !== 0) {
+    middle = array.slice(Math.floor(array.length / 2), Math.floor(array.length / 2) + 1);
+    return middle;
+  }
+};
+
+const testArray = [1, 2, 3, 4];
+middle(testArray);
+assertArraysEqual(testArray, [1, 2, 3, 4], true); //Should PASS
+
+const testArray2 = [1, 2, 3];
+assertArraysEqual(middle(testArray2), [2], true); //Should PASS
+
+const testArray3 = [1, 2];
+assertArraysEqual(middle(testArray3), [], true); //Should PASS
+
+const testArray4 = [1];
+assertArraysEqual(middle(testArray4), [], true); //Should PASS
+
