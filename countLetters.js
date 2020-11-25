e366e8f2a3accba0054155d1c30f9e170b599a2f
Rene Wang <rene.wang1@hotmail.com>
2020-11-25 18:54:29 +0000

    completed working countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -5,3 +5,18 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
   }
 };
+
+const countLetters = function(sentence) {
+  const count = {};
+  for (letter of sentence) {
+    if (letter in count) {
+      count[letter] += 1;
+    } else {
+      count[letter] = 1;
+    }
+  }
+  return count;
+}
+
+
+console.log(countLetters('LHL'))
