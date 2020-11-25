0ae53c2941c4bb53477601db919a1b9f838a2ce5
Rene Wang <rene.wang1@hotmail.com>
2020-11-25 19:42:12 +0000

    added test cases and fixed bugs in making result an object with numbers as values rather than strings

--- letterPositions.js
+++ letterPositions.js
@@ -1,15 +1,36 @@
+const eqArrays = function(array1, array2) {
+  let equal = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      equal = false;
+    }
+  }
+  return equal;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log('These arrays are equal!');
+  } else {
+    console.log('These arrays are not equal!');
+  }
+};
+
 const letterPositions = function(sentence) {
   const results = {};
-  for (i in sentence) {
+  for (const i in sentence) {
     if (results[sentence[i]]) {
-      results[sentence[i]].push(i);
+      results[sentence[i]].push(Number(i));
     } else {
       results[sentence[i]] = [];
-      results[sentence[i]].push(i);
+      results[sentence[i]].push(Number(i));
     }
   }
   delete results[' '];
   return results;
 };
 
-console.log(letterPositions('good morning'))
+const sentence = 'good morning';
+const result1 = letterPositions(sentence); //
+assertArraysEqual(result1['g'], [0, 11], true);
+assertArraysEqual(result1['o'], [1, 2, 6], true);
