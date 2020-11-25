ed6355e57c647e7526b21355e0be1d6fe6463f21
Rene Wang <rene.wang1@hotmail.com>
2020-11-25 20:51:53 +0000

    fixed bug with eqarray not checking length of both arrays were the same

--- eqArrays.js
+++ eqArrays.js
@@ -8,6 +8,9 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
   let equal = true;
+  if (array1.length !== array2.length) {
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       equal = false;
