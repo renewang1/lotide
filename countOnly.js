6a23e13826c1cc275d8c6d0fb0424952d2da9cf8
Rene Wang <rene.wang1@hotmail.com>
2020-11-25 18:36:47 +0000

    finished countOnly function

--- countOnly.js
+++ countOnly.js
@@ -7,7 +7,17 @@ const assertEqual = function(actual, expected) {
 };
 
 const countOnly = function(allItems, itemsToCount) {
-
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
 }
 
 const firstNames = [
