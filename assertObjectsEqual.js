992c789565c384264c854853e63697ba30e4f789
Rene Wang <rene.wang1@hotmail.com>
2020-11-25 20:57:40 +0000

    added eqObjects into assertObjectEqual

---
+++ assertObjectsEqual.js
@@ -0,0 +1,29 @@
+const eqArrays = function(array1, array2) {
+  let equal = true;
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return equal;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    for (const key in object1) {
+      if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
