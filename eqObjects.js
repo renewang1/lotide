3661860e79e4d120daeed64e4a067140b703e2b8
Rene Wang <rene.wang1@hotmail.com>
2020-11-25 20:04:24 +0000

    eqObjects functional for primitive value case

--- eqObjects.js
+++ eqObjects.js
@@ -5,3 +5,31 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
   }
 };
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    for (const key in object1) {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, ba)); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+console.log(eqObjects(ab, abc)); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+eqObjects(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+eqObjects(cd, cd2); // => false
