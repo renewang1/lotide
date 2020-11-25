c02c8e858eaf170afe0ba67956b28c87fb7f9a47
Rene Wang <rene.wang1@hotmail.com>
2020-11-25 19:57:29 +0000

    added test case and linted

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,3 +6,23 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const findKeyByValue = function(object, value) {
+  let result = undefined;
+  for (const key of Object.keys(object)) {
+    if (object[key] === value) {
+      result = key;
+    }
+  }
+  return result;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "T"), undefined);
