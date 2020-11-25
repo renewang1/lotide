60d6071812bd8d44a21110752051798aa9b84746
Rene Wang <rene.wang1@hotmail.com>
2020-11-25 18:25:13 +0000

    Created file countOnly.js and added assertEqual function for test cases

---
+++ countOnly.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
