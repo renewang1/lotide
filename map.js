86ebdf13d82aef2ec4391fe052244f3cd07350eb
Rene Wang <rene.wang1@hotmail.com>
2020-11-26 19:43:58 +0000

    added two more test cases

--- map.js
+++ map.js
@@ -34,3 +34,11 @@ console.log(results1);
 const array1 = ['hello', 'hi', 'hey'];
 const results2 = map(array1, word => word + ' there');
 assertArraysEqual(results2, ['hello there', 'hi there', 'hey there']);
+
+const array2 = [1, 2, 3];
+const results3 = map(array2, num => num * 2);
+assertArraysEqual(results3, [2, 4, 6]);
+
+const array3 = [];
+const results4 = map(array3, word => word[0]);
+assertArraysEqual(results4, []);
