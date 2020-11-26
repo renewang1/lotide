d5f4f49d0113e48d732777f827a41b245daeb9cd
Rene Wang <rene.wang1@hotmail.com>
2020-11-26 20:59:37 +0000

    started function but not working yet

--- findKey.js
+++ findKey.js
@@ -5,3 +5,23 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
   }
 };
+
+const findKey = function(object, callback) {
+  let found = undefined;
+  for (item in object) {
+    if (callback(item)) {
+      found = item;
+      return found;
+    }
+  }
+  return found;
+}
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)) // => "noma"
