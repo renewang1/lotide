a351acffff3bad58b75bacefbad3c7a417a266c9
Rene Wang <rene.wang1@hotmail.com>
2020-11-26 20:43:23 +0000

    added assertArraysEqual.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,21 @@
+const eqArrays = function(array1, array2) {
+  let equal = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      equal = false;
+    }
+  }
+  return equal;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: [${array1}] !== [${array2}]`);
+  }
+};
+
 const takeUntil = function(array, callback) {
   const results = [];
   for (let item of array) {
