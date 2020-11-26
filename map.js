532d4c7800d97a20941944b70a7386448dd6997a
Rene Wang <rene.wang1@hotmail.com>
2020-11-26 19:37:14 +0000

    added for loop in map function

---
+++ map.js
@@ -0,0 +1,16 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    console.log('item BEFORE: ', item);
+    console.log('item AFTER: ', callback(item));
+    console.log('---');
+  }
+  return results;
+}
+
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
