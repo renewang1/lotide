501eb24a2dd72f377443c29420cdd4df0ee15241
Rene Wang <rene.wang1@hotmail.com>
2020-11-27 01:20:19 +0000

    added test cases

--- findKey.js
+++ findKey.js
@@ -15,7 +15,7 @@ const findKey = function(object, callback) {
     }
   }
   return found;
-}
+};
 
 console.log(findKey({
   "Blue Hill": { stars: 1 },
@@ -24,4 +24,26 @@ console.log(findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)) // => "noma"
+}, x => x.stars === 2)); // => "noma"
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1);
+
+assertEqual(result, 'Blue Hill');
+
+const result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 0);
+
+assertEqual(result2, undefined);
