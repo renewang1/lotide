c8aaf8f06739b4742c3d59775b96830ed26f113a
Rene Wang <rene.wang1@hotmail.com>
2020-11-30 18:56:17 +0000

    test file for eqArrays

---
+++ test/eqArraysTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([], []), true); // => should PASS
+assertEqual(eqArrays([3, 2, 1], [1, 2, 3]), false); // => should FAIL
+assertEqual(eqArrays([3, 2, 1], [1]), false); // => should FAIL
+
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true); // => should PASS
+assertEqual(eqArrays(['1', '2', '3'], [1, 2, 3]), false); // => should FAIL
