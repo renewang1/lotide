18d5db237175310408a1f4223c3d47153e4afc69
Rene Wang <rene.wang1@hotmail.com>
2020-11-30 20:21:35 +0000

    linted findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -17,7 +17,7 @@ const findKeyByValue = function(object, value) {
 };
 
 const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
+  sciFi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
