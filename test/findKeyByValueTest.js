550c4277efe59f8f8175b9a0348d7a91d7d5842f
Rene Wang <rene.wang1@hotmail.com>
2020-12-01 16:12:19 +0000

    completed test case file for findKeyByValue

---
+++ test/findKeyByValueTest.js
@@ -0,0 +1,24 @@
+const { expect } = require('chai');
+const findKeyByValue = require('../findKeyByValue');
+
+describe('#findKeyByValue', () => {
+  const bestTVShowsByGenre = {
+    sciFi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+  
+  it("should return 'drama' for 'The Wire", () => {
+    expect(findKeyByValue(bestTVShowsByGenre, 'The Wire')).to.equal('drama');
+  });
+
+  it("should return undefined for value not in the object", () => {
+    try{
+      findKeyByValue(bestTVShowsByGenre, "That '70s Show");
+    } catch (err) {
+      expect(err.message).to.equal(undefined);
+    }
+  });
+
+});
+
