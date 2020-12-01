53d3827970e1bde0ac28a1c676da371b0112c25c
Rene Wang <rene.wang1@hotmail.com>
2020-12-01 16:11:54 +0000

    modularized findKeyByValue and created test file

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKeyByValue = function(object, value) {
   let result = undefined;
   for (const key of Object.keys(object)) {
@@ -17,14 +9,3 @@ const findKeyByValue = function(object, value) {
 };
 
 module.exports = findKeyByValue;
-
-const bestTVShowsByGenre = {
-  sciFi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "T"), undefined);
