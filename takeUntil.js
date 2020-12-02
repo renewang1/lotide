2145300b5cb749a73f34465635beae00c3ec4e49
Rene Wang <rene.wang1@hotmail.com>
2020-12-02 03:28:53 +0000

    modularized takeUntil and created test file

--- takeUntil.js
+++ takeUntil.js
@@ -1,21 +1,3 @@
-const eqArrays = function(array1, array2) {
-  let equal = true;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      equal = false;
-    }
-  }
-  return equal;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion passed: [${array1}] === [${array2}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: [${array1}] !== [${array2}]`);
-  }
-};
-
 const takeUntil = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -28,17 +10,3 @@ const takeUntil = function(array, callback) {
 };
 
 module.exports = takeUntil;
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-const data3 = [];
-const results3 = takeUntil(data3, x => x[0]);
-assertArraysEqual(results3, []);
---
+++ test/takeUntilTest.js
@@ -0,0 +1,13 @@
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+const data3 = [];
+const results3 = takeUntil(data3, x => x[0]);
+assertArraysEqual(results3, []);
