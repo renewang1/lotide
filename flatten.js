3c6b95306c1678686cab04121ffb799faf4de718
Rene Wang <rene.wang1@hotmail.com>
2020-12-02 03:04:52 +0000

    added test case file and modularized flatten

--- flatten.js
+++ flatten.js
@@ -1,21 +1,3 @@
-const eqArrays = function(array1, array2) {
-  let equal = true;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      equal = false;
-    }
-  }
-  return equal;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log('These arrays are equal!');
-  } else {
-    console.log('These arrays are not equal!');
-  }
-};
-
 const flatten = function(array) {
   let newArray = [];
   for (let value of array) {
@@ -31,10 +13,3 @@ const flatten = function(array) {
 };
 
 module.exports = flatten;
-
-console.log(flatten([1, 2, [1, 2, 3]]));
-
-const testArray = [1, 2, [1, 2, 3]];
-flatten(testArray);
-assertArraysEqual(testArray, [1, 2, [1, 2, 3]], true); // Should PASS
-
