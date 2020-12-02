39cb6580f9337dc22fd09c204c773debefbcb32c
Rene Wang <rene.wang1@hotmail.com>
2020-12-02 03:27:58 +0000

    finished test file for map

--- test/mapTest.js
+++ test/mapTest.js
@@ -1,15 +1,24 @@
+const { expect } = require('chai');
+const map = require('../map');
 
-const results1 = map(words, word => word[0]);
-console.log(results1);
+describe('#map', () => {
 
-const array1 = ['hello', 'hi', 'hey'];
-const results2 = map(array1, word => word + ' there');
-assertArraysEqual(results2, ['hello there', 'hi there', 'hey there']);
+  it("should return array of strings with word added to them", () => {
+    const array1 = ['hello', 'hi', 'hey'];
+    const results1 = map(array1, word => word + ' there');
+    expect(results1).to.deep.equal(['hello there', 'hi there', 'hey there']);
+  });
 
-const array2 = [1, 2, 3];
-const results3 = map(array2, num => num * 2);
-assertArraysEqual(results3, [2, 4, 6]);
+  it("should return array of numbers doubled", () => {
+    const array2 = [1, 2, 3];
+    const results2 = map(array2, num => num * 2);
+    expect(results2).to.deep.equal([2, 4, 6]);
+  })
 
-const array3 = [];
-const results4 = map(array3, word => word[0]);
-assertArraysEqual(results4, []);
+  it("should return empty array no matter what function it takes", () => {
+    const array3 = [];
+    const results3 = map(array3, word => word[0]);
+    expect(results3).to.deep.equal([]);
+  })
+
+});
