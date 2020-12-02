83b42a2302479a4cc1eb68cd16c39020cd8bd6d0
Rene Wang <rene.wang1@hotmail.com>
2020-12-02 03:34:56 +0000

    modularized without and added test file

--- without.js
+++ without.js
@@ -1,21 +1,3 @@
-const eqArrays = function(array1, array2) {
-  let equal = true;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      equal = false;
-    }
-  }
-  return equal;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log('These arrays are equal!');
-  } else {
-    console.log('These arrays are not equal!');
-  }
-};
-
 const without = function(source, itemsToRemove) {
   newArray = [];
   for (let value of source) {
@@ -27,18 +9,3 @@ const without = function(source, itemsToRemove) {
 };
 
 module.exports = without;
-
-console.log(without([1, 2, 3], [1])) // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
-
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
-testArray = [1, 2, 3];
-without(testArray, [1, 2]);
-assertArraysEqual(testArray, [1, 2, 3]); // Should PASS
-
-assertArraysEqual(without([1, 2, 3], []), [1, 2, 3]); // Should PASS
-assertArraysEqual(without([], [1, 2, 3]), []); // Should PASS
