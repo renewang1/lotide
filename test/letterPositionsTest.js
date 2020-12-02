7612d42a31aaff7b02fd7590a7a8e4286a57d565
Rene Wang <rene.wang1@hotmail.com>
2020-12-02 03:15:47 +0000

    finished test cases

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -1,7 +1,15 @@
+const { expect } = require('chai');
+const letterPositions = require('../letterPositions');
 
-module.exports = letterPositions;
+describe('#letterPositions', () => {
+  const sentence = 'good morning';
+  const result = letterPositions(sentence);
 
-const sentence = 'good morning';
-const result1 = letterPositions(sentence); //
-assertArraysEqual(result1['g'], [0, 11], true);
-assertArraysEqual(result1['o'], [1, 2, 6], true);
+  it("should return [0, 11] for ['g'] in 'good morning", () => {
+    expect(result['g']).to.deep.equal([0, 11]);
+  });
+
+  it("should return [1, 2, 6] for ['o'] in 'good morning", () => {
+    expect(result['o']).to.deep.equal([1, 2, 6]);
+  });
+});
